9f9e8fc89c209491c3b76d5de423ee9f0048f71c
FrankFonts <9556806+FrankFonts@users.noreply.github.com>
2022-07-01 22:13:31 +0200

    Add new task, reset list for real

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
-window.addEventListener('load', initApp());
+window.addEventListener('load', updateApp);
+
+document.addEventListener('click', manageClicks);
+
+function manageClicks(e) {
+    let element = e.target.closest('button');
+    if (element === null) return;
+
+    let action = element.getAttribute('data-action');
+
+    if (action === 'reset') {
+        toDoList = [];
+        removeFromLocalStorage('toDoList');
+    }
+    if (action === 'add-item') {
+        let toDoString = readData();
+        updateList(toDoString);
+        writeToLocalStorage('toDoList', toDoList);
+    }
+    updateApp();
+}
+
+function readData() {
+    let toDoString = document.querySelector('#to-do-text').value;
+    console.log(toDoString);
+    return toDoString;
+
+}
+
+function updateList(string) {
+    let obj = { 'title': string };
+    obj['date'] = new Date();
+    obj['status'] = 'undone';
+    toDoList.push(obj);
+}
 
 let toDoList = [{
     "title": "test to do item",
@@ -12,9 +46,17 @@ let toDoList = [{
 
 
 
-function initApp() {
+function updateApp() {
     let toDoList = readFromLocalStorage('toDoList');
-    (toDoList && toDoList !== []) ? renderList(toDoList): renderMessage('Nothing to do');
+    let message = document.querySelector('.app__message');
+
+    renderList(toDoList);
+
+    if (toDoList) {
+        message.classList.add('hidden');
+    } else {
+        message.classList.remove('hidden');
+    }
 }
 
 function writeToLocalStorage(key, value) {
@@ -30,18 +72,19 @@ function readFromLocalStorage(key) {
 }
 
 function renderList(list) {
-    list.forEach(item => {
-        // console.log(item);
-        document.querySelector('.app__message').append(toDoElement(item.title, item.date, item.status));
-    });
+    let contentElement = document.querySelector('.app__content')
+    contentElement.innerHTML = '';
+
+    if (list && list !== []) {
+        list.forEach(item => {
+            document.querySelector('.app__content').append(toDoElement(item.title, item.date, item.status));
+        });
+    }
 }
 
-function renderMessage(message) {
-    document.querySelector('.app__message').innerHTML = message;
-}
 
 function toDoElement(title, date, status) {
-    let element = createElement('div', 'item', '');
+    let element = createElement('div', 'app__item', '');
     element.append(createElement('div', 'title', title));
     element.append(createElement('div', 'date', date));
     element.append(createElement('div', 'status', status));
