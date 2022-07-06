467fac1e111975908b7dd571f37156448b08a423
FrankFonts <9556806+FrankFonts@users.noreply.github.com>
2022-07-06 23:13:54 +0200

    Delete task - works, code is messy

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let toDoList = readFromLocalStorage('toDoList');
+
 window.addEventListener('load', updateApp);
 
 document.addEventListener('click', manageClicks);
@@ -8,41 +10,54 @@ function manageClicks(e) {
 
     let action = element.getAttribute('data-action');
 
+    console.log(action);
+
     if (action === 'reset') {
         toDoList = [];
         removeFromLocalStorage('toDoList');
     }
     if (action === 'add-item') {
-        let toDoString = readData();
-        updateList(toDoString);
+        let toDoString = readUserInput();
+        updateList(toDoString, toDoList);
+        writeToLocalStorage('toDoList', toDoList);
+    }
+    if (action === 'delete-item') {
+        let id = getItemId(element);
+        deleteItemFromList(id, toDoList);
         writeToLocalStorage('toDoList', toDoList);
     }
     updateApp();
 }
 
-function readData() {
-    let toDoString = document.querySelector('#to-do-text').value;
-    console.log(toDoString);
-    return toDoString;
 
+function getItemId(element) {
+    return element.closest('.app__item').getAttribute('data-id');
 }
 
-function updateList(string) {
+function deleteItemFromList(id, list) {
+    list.forEach(function(listItem, index) {
+        if (listItem['id'] === id) {
+            list.splice(index, 1);
+        }
+    })
+}
+
+function readUserInput() {
+    let toDoString = document.querySelector('#to-do-text').value;
+    return toDoString;
+}
+
+function updateList(string, list) {
     let obj = { 'title': string };
     obj['date'] = new Date();
     obj['status'] = 'undone';
-    toDoList.push(obj);
+
+    let id = (Math.floor(Math.random() * 1000000000000000)).toString();
+    obj['id'] = id;
+    list.push(obj);
+
 }
 
-let toDoList = [{
-    "title": "test to do item",
-    "date": "2022-06-23",
-    "status": "done"
-}, {
-    "title": "another to do item",
-    "date": "2022-06-29",
-    "status": "undone"
-}]
 
 
 
@@ -68,7 +83,7 @@ function removeFromLocalStorage(key) {
 }
 
 function readFromLocalStorage(key) {
-    return JSON.parse(window.localStorage.getItem(key));
+    return JSON.parse(window.localStorage.getItem(key)) || [];
 }
 
 function renderList(list) {
@@ -77,17 +92,39 @@ function renderList(list) {
 
     if (list && list !== []) {
         list.forEach(item => {
-            document.querySelector('.app__content').append(toDoElement(item.title, item.date, item.status));
+            // document.querySelector('.app__content').append(toDoElement(item.title, item.date, item.status));
+            document.querySelector('.app__content').innerHTML +=
+                `
+                <div class="app__item" data-id="${item.id}">
+                    <div class="title">
+                        ${item.title}
+                    </div>
+                    <div class="date">
+                        ${item.date}
+                    </div>
+                    <div class="status">
+                        ${item.status}
+                    </div>
+                    <div class="done">
+                        <button type="button" data-action="set-item-status">Item done</button>
+                    </div>
+                    <div class="delete">
+                        <button type="button" data-action="delete-item">Delete item</button>
+                    </div>
+                </div>
+                `
         });
     }
 }
 
 
 function toDoElement(title, date, status) {
-    let element = createElement('div', 'app__item', '');
-    element.append(createElement('div', 'title', title));
-    element.append(createElement('div', 'date', date));
-    element.append(createElement('div', 'status', status));
+    // let element = createElement('div', 'app__item', '');
+    // element.append(createElement('div', 'title', title));
+    // element.append(createElement('div', 'date', date));
+    // element.append(createElement('div', 'status', status));
+
+    // element.append(createElement('button', 'delete', "Delete"));
 
     return element;
 }
